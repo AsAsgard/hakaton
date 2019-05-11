2deaaeb421aad6cd39fe4696df4e9a1ac042ac03
Andrew G <andrew@localhost.localdomain>
2019-05-12 00:45:04 +0300

    minor fixes

--- vk_bot/ml_functions/predict.py
+++ vk_bot/ml_functions/predict.py
@@ -10,24 +10,29 @@ w2v = load_w2v_model('./data/w2v/my_model')
 annoy_model = load_annoy_model('./data/annoy')
 df = load_dataset('./data/ProductsDataset.csv')
 
+
 @lru_cache(maxsize=1000000)
 def get_normal_form(i):
     return morph.normal_forms(i)[0]
 
+
 def normalize_text(x):
     return ' '.join([get_normal_form(i) for i in re.findall('\w+', x)])
+
 
 def rem_spaces(x):
     return re.sub(' +', ' ', x).strip()
 
+
 def rem_punctuation(x):
     return re.sub('[^A-Za-zА-Яа-я0-9 ]+', '', x)
+
 
 def get_good_text(x):
     return rem_punctuation(rem_spaces(normalize_text(x)))
 
 
-df['title_proc'] = df['title'].apply(get_good_text)    
+df['title_proc'] = df['title'].apply(get_good_text)
 df['descrirption'] = df['descrirption'].apply(get_good_text)
 df['all_desc'] = df['title_proc'] + ' ' + df['descrirption']
 
@@ -41,7 +46,7 @@ def get_avg_vector(text):
     idfsum = 0
     for i in range(length):
         ix = vec.vocabulary_.get(splitted[i], None)
-        if not ix is None:
+        if ix is not None:
             idf = vec.idf_[ix]
         else:
             idf = 1
@@ -58,6 +63,7 @@ def get_index(text):
     annoy_res = annoy_model.get_nns_by_vector(normed, 10)
     return annoy_res
 
+
 def get_results(idx):
     titles = df['title'][idx].values
     product_ids = df['product_id'][idx].values
@@ -66,9 +72,10 @@ def get_results(idx):
     for title, ix, img in zip(titles, product_ids, imgs):
         link = None if img == 'nan' else img
         tmp = {'title': title, 'product_id': ix, 'image': link}
-        res.append(tmp)    
-    
+        res.append(tmp)
+
     return res
+
 
 def process_data(input_text):
     idx = get_index(input_text)
--- vk_bot/ml_functions/processing.py
+++ vk_bot/ml_functions/processing.py
@@ -2,14 +2,23 @@ import pandas as pd
 from annoy import AnnoyIndex
 from gensim.models import KeyedVectors
 from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
+
+
+nltk.download("stopwords")
+sw = stopwords.words("russian")
+
 
 def load_w2v_model(path_to_model):
     return KeyedVectors(300).load(path_to_model)
+
 
 def load_annoy_model(path_to_model):
     annoy_model = AnnoyIndex(300)
     annoy_model.load(path_to_model)
     return annoy_model
+
 
 def load_dataset(path_to_data):
     df = pd.read_csv(path_to_data)
@@ -18,8 +27,8 @@ def load_dataset(path_to_data):
     df['product_id'] = range(df.shape[0])
     return df
 
+
 def get_tfidf_model(df):
-    vec = TfidfVectorizer()
+    vec = TfidfVectorizer(stop_words=sw)
     vec.fit_transform(df['all_desc'])
     return vec
-
